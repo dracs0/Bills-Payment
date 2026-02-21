3922bb910852445cb28b884572960fab29cbec70
arbys27 <99326044+dracs0@users.noreply.github.com>
2026-02-21 17:35:30 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            String username = "Renmar";
+            String password = "Renmar123";
+
+            Console.WriteLine("Bills Payment");
+
+            Console.WriteLine("Enter your Name: ");
+            string inputUsername = Console.ReadLine();
+
+            Console.WriteLine("Enter your Password: ");
+            string inputPassword = Console.ReadLine();
+                        
+            for (int i = 0; i < 2; i++)
+            {
+                if (inputUsername == username && inputPassword == password)
+                {
+                    Console.WriteLine("Login successful! Welcome, " + username);
+
+                    Console.WriteLine("Enter an amount: ");
+                    string inputAmount = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password. please try again.");
+                }
+            }
         }
     }
 }
