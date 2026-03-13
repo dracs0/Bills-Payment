b62491c6370999ccd9e77bab90136eb555b9bbc3
dracs0 <johnrenmar2003@gmail.com>
2026-03-13 19:18:48 +0800

    Initial commit
    confused on how to manage things especially the diagram one.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
-﻿namespace Bill_Payment
+﻿using System;
+using BillPaymentModels;
+using BillPaymentAppService;
+
+namespace Bill_Payment
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             for (int i = 0; i < 3; i++)
             {
 
-            Console.WriteLine("Bills Payment");
+                Console.WriteLine("Bills Payment");
 
-            Console.WriteLine("Enter your Name: ");
-            string inputUsername = Console.ReadLine();
+                Console.WriteLine("Enter your Name: ");
+                string inputUsername = Console.ReadLine();
 
-            Console.WriteLine("Enter your Password: ");
-            string inputPassword = Console.ReadLine();
-                       
+                Console.WriteLine("Enter your Password: "); 
+                string inputPassword = Console.ReadLine();
+
                 if (inputUsername == "Renmar" && inputPassword == "Renmar123")
                 {
                     Console.WriteLine("Login successful! Welcome, " + inputUsername + " !");
@@ -28,11 +32,20 @@
                     Console.WriteLine("Payment Method (Credit/Debit): ");
                     string paymentMethod = Console.ReadLine();
 
-                    Console.WriteLine("Bill Type (Water/Electricity): ");
+                    Console.WriteLine("Bill Type (Water/Electricity/Internet): ");
                     string billType = Console.ReadLine();
 
-                    Console.WriteLine(billType + " bill of " + inputAmount + " paid successfully using " + paymentMethod + ". Thank you for your payment!");
+                    Models payment = new Models
+                    {
+                        PaymentId = Guid.NewGuid(),
+                        Username = inputUsername,
+                        Bank = bank,
+                        Amount = inputAmount,
+                        PaymentMethod = paymentMethod,
+                        BillType = billType
+                    };
 
+                    Console.WriteLine(payment.BillType + " bill of " + payment.Amount + " paid successfully using " + payment.PaymentMethod + " through " + payment.Bank + ". Thank you for your payment!");
                     break;
                 }
 
